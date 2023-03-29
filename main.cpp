e72915581f1d1a37576e9145f942a81fd457f3a2
Snocci <128066678+Snocci@users.noreply.github.com>
2023-03-29 14:49:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+    float a=0;
+    float b=0;
+    float c=0;
+    float Triangolo, Quadrato, Rettangolo, Trapezio;
+    cout << "Insersci tre valori" << endl;
 
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+    Triangolo= (a*b)/2;
+    Quadrato= a*a;
+    Rettangolo= a*b;
+    Trapezio= ((a+b)*c)/2;
+    cout <<"Area del triangolo=" << Triangolo << endl;
+    cout << "Area del quadrato=" << Quadrato << endl;
+    cout << "Area del rettangolo=" << Rettangolo << endl;
+    cout << "Area del trapezio=" << Trapezio << endl;
+    return 0;
 
 
 }
